ec0714defb15fc71802e990c8a620e8503abafa1
Shubham9088 <shubhamchakole22@gmail.com>
2022-01-30 15:17:47 +0530

    2nd case

--- test menu/src/test/acts/Testclass.java
+++ test menu/src/test/acts/Testclass.java
@@ -13,12 +13,20 @@ public class Testclass {
 		 System.out.println("Enter your choice from the following menu:");
 		 System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Exit");
 		 option = sc.nextInt();
-		
+		 if(option!=5){
+			 System.out.println("Enter the first number");
+			 num1=sc.nextInt();
+			 System.out.println("Enter the second number");
+			 num2=sc.nextInt();
+			 }
+			 else
+			 break;
 		 switch(option)
 		 {
 		 case 1:System.out.println("Addition of "+num1+" and "+num2+" is "+(num1+num2));
 		 break;
-		
+		 case 2:System.out.println("Subtraction of "+num1+" and "+num2+" is "+(num1-num2));
+		 break;
 		 default: System.out.println("Invalid choice");
 		 }
 		 System.out.println("Do you want to continue?1.Yes 2.No");
